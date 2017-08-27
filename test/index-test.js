55d08d12d1710921e2f5bd901a360c33fe466eaf
Adiel Wilson <adielwilson@gmail.com>
2017-08-27 02:51:34 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,25 @@
+const expect = require('expect')
+
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+var uppercase = "I LOVE YOU GRANDMA"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+
+function shout(string) {
+  return string.toUpperCase()
+}
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
